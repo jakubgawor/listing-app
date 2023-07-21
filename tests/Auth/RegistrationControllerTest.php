25ab2380194005a638e97d9f77853303f4a81eda
Jakub Gawor <testetest@test.pl>
2023-07-21 21:56:54 +0200

    added registration tests

--- tests/Auth/RegistrationControllerTest.php
+++ tests/Auth/RegistrationControllerTest.php
@@ -2,8 +2,8 @@
 
 namespace App\Tests\Auth;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-use Symfony\Component\HttpFoundation\Response;
 
 class RegistrationControllerTest extends WebTestCase
 {
@@ -12,9 +12,36 @@ class RegistrationControllerTest extends WebTestCase
         $client = static::createClient();
         $crawler = $client->request('GET', '/register');
 
-        $this->assertSame(Response::HTTP_OK, $client->getResponse()->getStatusCode());
+        $this->assertResponseIsSuccessful();
+    }
+
+    public function testUserCanRegisterWithValidForm()
+    {
+        $client = static::createClient();
+
+        $uniqueId = uniqid();
+
+        $client->request('POST', '/register', [
+            'registration_form' => [
+                'email' => 'test_email_' . $uniqueId . '@example.com',
+                'username' => 'test_username' . $uniqueId,
+                'plainPassword' => [
+                    'first' => 'test_password' . $uniqueId,
+                    'second' => 'test_password' . $uniqueId
+                ],
+                'agreeTerms' => '1',
+            ]]);
 
 
-//        $this->assertResponseIsSuccessful();
+        $this->assertSame(302, $client->getResponse()->getStatusCode());
+
+        $entityManager = static::getContainer()->get('doctrine')->getManager();
+
+        $user = $entityManager->getRepository(User::class)->findOneBy([
+            'email' => 'test_email_' . $uniqueId . '@example.com',
+            'username' => 'test_username' . $uniqueId
+        ]);
+
+        $this->assertNotNull($user);
     }
 }
