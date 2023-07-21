7d017589a9f6eb451fe72196409ef038ace67920
Jakub Gawor <testetest@test.pl>
2023-07-22 00:11:39 +0200

    added one more registration test

--- tests/Auth/RegistrationControllerTest.php
+++ tests/Auth/RegistrationControllerTest.php
@@ -4,6 +4,9 @@ namespace App\Tests\Auth;
 
 use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpFoundation\Session\Storage\MockArraySessionStorage;
 
 class RegistrationControllerTest extends WebTestCase
 {
@@ -43,5 +46,31 @@ class RegistrationControllerTest extends WebTestCase
         ]);
 
         $this->assertNotNull($user);
+    }
+
+    public function testRegistrationWithInvalidEmail(): void
+    {
+        $client = static::createClient();
+
+        $session = new Session(new MockArraySessionStorage());
+        $request = Request::createFromGlobals();
+        $request->setSession($session);
+
+        $uniqueId = uniqid();
+
+        $crawler = $client->request('POST', '/register', [
+            'registration_form' => [
+                'email' => 'invalid_email',
+                'username' => 'test_username' . $uniqueId,
+                'plainPassword' => [
+                    'first' => 'test_password' . $uniqueId,
+                    'second' => 'test_password' . $uniqueId
+                ],
+                'agreeTerms' => '1',
+            ]]);
+
+
+        $this->assertNotNull($session->getFlashBag()->get('verify_email_error'));
+        $this->assertSame(200, $client->getResponse()->getStatusCode());
     }
 }
