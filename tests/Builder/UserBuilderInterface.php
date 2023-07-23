68bfa61a3672ab4a41c9f0c1e92342183933044e
Jakub Gawor <testetest@test.pl>
2023-07-23 12:25:35 +0200

    refactoring

--- tests/Builder/UserBuilderInterface.php
+++ tests/Builder/UserBuilderInterface.php
@@ -3,10 +3,6 @@
 namespace App\Tests\Builder;
 
 use App\Entity\User;
-use App\Entity\UserProfile;
-use App\Enum\UserRoleEnum;
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 interface UserBuilderInterface
 {
