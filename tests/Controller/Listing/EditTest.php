d4e05a47bcc7d125b4f656cc9462c1951f3997d1
Jakub Gawor <testetest@test.pl>
2023-08-03 00:52:19 +0200

    refactored edit listing tests and user profile tests adapted to updated create user builder

---
+++ tests/Controller/Listing/EditTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Tests\Controller\Listing;
+
+use App\Entity\Listing;
+use App\Enum\ListingStatusEnum;
+use App\Tests\Builder\EntityBuilder;
+use Doctrine\ORM\EntityRepository;
+use Faker\Factory;
+use Faker\Generator;
+
+class EditTest extends EntityBuilder
+{
+    private EntityRepository $repository;
+    private Generator $faker;
+
+    public function setUp(): void
+    {
+        $this->repository = static::getContainer()->get('doctrine')->getManager()->getRepository(Listing::class);
+        $this->faker = Factory::create();
+
+        self::ensureKernelShutdown();
+    }
+
+    public function testUserCanEditHisOwnListing(): void
+    {
+        $client = static::createClient();
+        $author = $this->createUser();
+        $client->loginUser($author);
+
+        $listing = $this->createListing($this->faker->realText(15), $this->faker->realText(20), ListingStatusEnum::VERIFIED, $author);
+
+        $oldSlug = $listing->getSlug();
+
+        $title = $this->faker->realText(15);
+        $description = $this->faker->realText(20);
+
+        $crawler = $client->request('GET', '/listing/' . $oldSlug . '/edit');
+        $form = $crawler->selectButton('Edit listing')->form([
+            'listing_form[title]' => $title,
+            'listing_form[description]' => $description
+        ]);
+        $client->submit($form);
+
+        /** @var Listing $editedListing */
+        $editedListing = $this->repository->findOneBy([
+            'title' => $title,
+            'description' => $description,
+            'belongs_to_user' => $author->getId()
+        ]);
+
+        $this->assertNull($this->repository->findOneBy([
+            'slug' => $oldSlug
+        ]));
+        $this->assertNotNull($editedListing);
+        $this->assertNotSame($editedListing->getSlug(), $oldSlug);
+        $this->assertNotNull($editedListing->getEditedAt());
+        $this->assertNotEmpty($client->getRequest()->getSession()->getFlashBag()->get('success'));
+        $this->assertResponseRedirects('/', 302);
+    }
+
+}
