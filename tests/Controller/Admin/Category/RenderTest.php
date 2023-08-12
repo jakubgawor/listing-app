8fa584840482cf676d8db9de701659bbe51de779
Jakub Gawor <testetest@test.pl>
2023-08-12 17:05:17 +0200

    added render tests

--- tests/Controller/Admin/Category/RenderTest.php
+++ tests/Controller/Admin/Category/RenderTest.php
@@ -4,16 +4,45 @@ namespace App\Tests\Controller\Admin\Category;
 
 use App\Enum\UserRoleEnum;
 use App\Tests\Builder\EntityBuilder;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 
 class RenderTest extends EntityBuilder
 {
     public function testCreateCategoryPageCanBeRendered(): void
     {
-        static::createClient()
-            ->loginUser($this->createUser(['role' => UserRoleEnum::ROLE_ADMIN]))
-            ->request('GET', '/admin/create-category');
+        $this->loginAndRequest('/admin/create-category');
 
         $this->assertResponseIsSuccessful();
+    }
+
+    public function testEditCategoryPageCanBeRenderedIfCategoryExists(): void
+    {
+        $client = static::createClient();
+
+        $admin = $this->createUser(['role' => UserRoleEnum::ROLE_ADMIN]);
+        $category = $this->createCategory(uniqid(), $admin);
+
+        $client
+            ->loginUser($admin)
+            ->request('GET', '/admin/category/' . $category->getId() . '/edit');
+
+        $this->assertResponseIsSuccessful();
+    }
+
+    public function testEditCategoryPageCanNotBeRenderedIfCategoryDoesNotExist(): void
+    {
+        $client = $this->loginAndRequest('/admin/category/not-existing/edit');
+
+        $this->assertSame(['Object not found'], $client->getRequest()->getSession()->getFlashBag()->get('error'));
+    }
+
+    private function loginAndRequest(string $uri): KernelBrowser
+    {
+        $client = static::createClient();
+        $client->loginUser($this->createUser(['role' => UserRoleEnum::ROLE_ADMIN]))
+            ->request('GET', $uri);
+
+        return $client;
     }
 
 }
