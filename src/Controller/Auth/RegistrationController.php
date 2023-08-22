c482ea75d0209b4c3c74f35930c7720921ce2ed5
Jakub Gawor <testetest@test.pl>
2023-08-22 21:12:46 +0200

    refactored RegistrationController

--- src/Controller/Auth/RegistrationController.php
+++ src/Controller/Auth/RegistrationController.php
@@ -3,78 +3,39 @@
 namespace App\Controller\Auth;
 
 use App\Entity\User;
-use App\Entity\UserProfile;
-use App\Enum\UserRoleEnum;
-use App\Form\Type\RegistrationFormType;
+use App\Form\Handler\EntityFormHandler;
 use App\Repository\UserRepository;
-use App\Security\EmailVerifier;
 use App\Security\LoginFormAuthenticator;
-use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use App\Service\AuthorizationService;
+use App\Service\RegistrationService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Mime\Address;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
 use Symfony\Contracts\Translation\TranslatorInterface;
-use SymfonyCasts\Bundle\VerifyEmail\Exception\VerifyEmailExceptionInterface;
 
 class RegistrationController extends AbstractController
 {
-    private EmailVerifier $emailVerifier;
-
-    public function __construct(EmailVerifier $emailVerifier)
+    public function __construct(
+        private readonly EntityFormHandler    $entityFormHandler,
+        private readonly RegistrationService  $registrationService,
+        private readonly AuthorizationService $authorizationService
+    )
     {
-        $this->emailVerifier = $emailVerifier;
     }
 
     #[Route('/register', name: 'app_register')]
-    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, UserAuthenticatorInterface $userAuthenticator, LoginFormAuthenticator $authenticator, EntityManagerInterface $entityManager): Response
+    public function register(Request $request, UserAuthenticatorInterface $userAuthenticator, LoginFormAuthenticator $authenticator): Response
     {
-        if ($this->getUser()) {
-            $this->addFlash('error','You are already logged in!');
-            return $this->redirectToRoute('app_index');
-        }
+        $this->authorizationService->denyLoggedUserAccess($this->getUser());
 
         $user = new User();
-        $form = $this->createForm(RegistrationFormType::class, $user);
-        $form->handleRequest($request);
+        $form = $this->entityFormHandler->handle($user, $request, $user, $this->registrationService);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $user->setPassword(
-                $userPasswordHasher->hashPassword(
-                    $user,
-                    $form->get('plainPassword')->getData()
-                )
-            );
-            $user->setRoles([UserRoleEnum::ROLE_USER]);
-            $entityManager->persist($user);
-
-//            creating an user profile
-            $userProfile = new UserProfile();
-            $userProfile->setUser($user);
-            $entityManager->persist($userProfile);
-
-            $entityManager->flush();
-
-            // generate a signed url and email it to the user
-            $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
-                (new TemplatedEmail())
-                    ->from(new Address('mailer@listing-app.com', 'Listing App'))
-                    ->to($user->getEmail())
-                    ->subject('Please Confirm your Email')
-                    ->htmlTemplate('registration/confirmation_email.html.twig')
-            );
-            // do anything else you need here, like send an email
-
+        if ($form === true) {
             $this->addFlash('notification', 'Check your inbox to verify your account email address!');
-            return $userAuthenticator->authenticateUser(
-                $user,
-                $authenticator,
-                $request
-            );
+            return $userAuthenticator->authenticateUser($user, $authenticator, $request);
         }
 
         return $this->render('registration/register.html.twig', [
@@ -86,29 +47,9 @@ class RegistrationController extends AbstractController
     public function verifyUserEmail(Request $request, TranslatorInterface $translator, UserRepository $userRepository): Response
     {
         $id = $request->query->get('id');
-
-        if (null === $id) {
-            return $this->redirectToRoute('app_register');
-        }
-
-        $user = $userRepository->find($id);
-
-        if (null === $user) {
-            return $this->redirectToRoute('app_register');
-        }
-
-        // validate email confirmation link, sets User::isVerified=true and persists
-        try {
-            $user->setRoles([UserRoleEnum::ROLE_USER_EMAIL_VERIFIED]);
-            $this->emailVerifier->handleEmailConfirmation($request, $user);
-        } catch (VerifyEmailExceptionInterface $exception) {
-            $this->addFlash('verify_email_error', $translator->trans($exception->getReason(), [], 'VerifyEmailBundle'));
-
-            return $this->redirectToRoute('app_register');
-        }
+        $this->registrationService->verifyEmailAddress($id, $request);
 
         $this->addFlash('success', 'Your email address has been verified!');
-
-        return $this->redirectToRoute('app_register');
+        return $this->redirectToRoute('app_index');
     }
 }
