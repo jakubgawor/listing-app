52faf4eeee5a5e75b3bd2644f1a30e556901b0a8
Jakub Gawor <testetest@test.pl>
2023-08-24 14:58:48 +0200

    updated RegistrationController

--- src/Controller/Auth/RegistrationController.php
+++ src/Controller/Auth/RegistrationController.php
@@ -3,8 +3,7 @@
 namespace App\Controller\Auth;
 
 use App\Entity\User;
-use App\Form\Handler\EntityFormHandler;
-use App\Repository\UserRepository;
+use App\Form\Handler\RegistrationFormHandler;
 use App\Security\LoginFormAuthenticator;
 use App\Service\AuthorizationService;
 use App\Service\RegistrationService;
@@ -13,14 +12,13 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
-use Symfony\Contracts\Translation\TranslatorInterface;
 
 class RegistrationController extends AbstractController
 {
     public function __construct(
-        private readonly EntityFormHandler    $entityFormHandler,
         private readonly RegistrationService  $registrationService,
-        private readonly AuthorizationService $authorizationService
+        private readonly AuthorizationService $authorizationService,
+        private readonly RegistrationFormHandler $registrationFormHandler
     )
     {
     }
@@ -30,8 +28,8 @@ class RegistrationController extends AbstractController
     {
         $this->authorizationService->denyLoggedUserAccess($this->getUser());
 
-        $user = new User();
-        $form = $this->entityFormHandler->handle($user, $request, $user, $this->registrationService);
+        $user = new User;
+        $form = $this->registrationFormHandler->handle($request, $user);
 
         if ($form === true) {
             $this->addFlash('notification', 'Check your inbox to verify your account email address!');
@@ -44,10 +42,9 @@ class RegistrationController extends AbstractController
     }
 
     #[Route('/verify/email', name: 'app_verify_email')]
-    public function verifyUserEmail(Request $request, TranslatorInterface $translator, UserRepository $userRepository): Response
+    public function verifyUserEmail(Request $request): Response
     {
-        $id = $request->query->get('id');
-        $this->registrationService->verifyEmailAddress($id, $request);
+        $this->registrationService->verifyEmailAddress($request);
 
         $this->addFlash('success', 'Your email address has been verified!');
         return $this->redirectToRoute('app_index');
