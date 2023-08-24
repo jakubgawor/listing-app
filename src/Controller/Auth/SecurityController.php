f8c1107c9872bb6a47ecb19ce056e731232f51fc
Jakub Gawor <testetest@test.pl>
2023-08-24 14:26:28 +0200

    refactoring

--- src/Controller/Auth/SecurityController.php
+++ src/Controller/Auth/SecurityController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Auth;
 
+use App\Service\AuthorizationService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,13 +10,16 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
+    public function __construct(
+        private readonly AuthorizationService $authorizationService
+    )
+    {
+    }
+
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-         if ($this->getUser()) {
-             $this->addFlash('error','You are already logged in!');
-             return $this->redirectToRoute('app_index');
-         }
+        $this->authorizationService->denyLoggedUserAccess($this->getUser());
 
         $error = $authenticationUtils->getLastAuthenticationError();
         $lastUsername = $authenticationUtils->getLastUsername();
