81afaf28307ccca94cb62e8923385fa71fb98f7d
Jakub Gawor <testetest@test.pl>
2023-08-31 17:30:52 +0200

    added AppConfigTest

---
+++ tests/Service/Config/AppConfigTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Tests\Service\Config;
+
+use App\Service\Config\AppConfig;
+use PHPUnit\Framework\TestCase;
+
+class AppConfigTest extends TestCase
+{
+    /** @test */
+    public function getBaseUrl_returns_correct_data()
+    {
+        $baseUrl = 'https://example.com/';
+        $config = new AppConfig($baseUrl);
+
+        $this->assertSame($baseUrl, $config->getBaseUrl());
+    }
+}
