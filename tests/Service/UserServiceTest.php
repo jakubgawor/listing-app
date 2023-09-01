38b4ed38e661373521ccd3e1bbd1551a3d4e6106
Jakub Gawor <testetest@test.pl>
2023-09-01 16:27:39 +0200

    added UserServiceTest

---
+++ tests/Service/UserServiceTest.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Tests\Service;
+
+use App\DTO\ChangePasswordDTO;
+use App\Entity\User;
+use App\Entity\UserProfile;
+use App\Enum\UserRoleEnum;
+use App\Exception\AdminDeletionException;
+use App\Service\EmailService;
+use App\Service\UserService;
+use Doctrine\ORM\EntityManagerInterface;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\PasswordHasher\Exception\InvalidPasswordException;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+
+class UserServiceTest extends TestCase
+{
+    private EntityManagerInterface $entityManager;
+    private TokenStorageInterface $tokenStorage;
+    private EmailService $emailService;
+    private UserPasswordHasherInterface $userPasswordHasher;
+    private UserService $userService;
+
+
+    public function setUp(): void
+    {
+        $this->entityManager = $this->createMock(EntityManagerInterface::class);
+        $this->tokenStorage = $this->createMock(TokenStorageInterface::class);
+        $this->emailService = $this->createMock(EmailService::class);
+        $this->userPasswordHasher = $this->createMock(UserPasswordHasherInterface::class);
+
+        $this->userService = new UserService(
+            $this->entityManager,
+            $this->tokenStorage,
+            $this->emailService,
+            $this->userPasswordHasher
+        );
+    }
+
+    /** @test */
+    public function deleteUser_throws_AdminDeletionException_if_user_is_admin()
+    {
+        $user = $this->createMock(User::class);
+        $user->method('getRoles')->willReturn([UserRoleEnum::ROLE_ADMIN]);
+
+        $this->expectException(AdminDeletionException::class);
+
+        $this->userService->deleteUser($user);
+    }
+
+    /** @test */
+    public function deleteUser_works_correctly_if_user_is_not_admin()
+    {
+        $user = $this->createMock(User::class);
+        $user->method('getRoles')->willReturn([UserRoleEnum::ROLE_USER_EMAIL_VERIFIED]);
+        $user->method('getUserProfile')->willReturn($this->createMock(UserProfile::class));
+
+        $this->entityManager->expects($this->exactly(2))->method('remove');
+        $this->tokenStorage->expects($this->once())->method('setToken')->with(null);
+        $this->entityManager->expects($this->once())->method('flush');
+
+        $this->userService->deleteUser($user);
+    }
+
+    /** @test */
+    public function changeEmail_works_correctly()
+    {
+        $user = $this->createMock(User::class);
+
+        $this->entityManager->expects($this->once())->method('persist')->with($user);
+        $this->entityManager->expects($this->once())->method('flush');
+        $this->emailService->expects($this->once())->method('sendRegistrationEmailConfirmation')->with($user);
+
+        $this->userService->changeEmail($user);
+    }
+
+    /** @test */
+    public function changePassword_throws_InvalidPassword_Exception_when_password_is_not_correct()
+    {
+        $user = $this->createMock(User::class);
+        $changePasswordDTO = $this->createMock(ChangePasswordDTO::class);
+        $this->userPasswordHasher->method('isPasswordValid')->willReturn(false);
+
+        $this->expectException(InvalidPasswordException::class);
+
+        $this->userService->changePassword($user, $changePasswordDTO);
+    }
+
+}
